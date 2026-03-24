488d3678a3319b436a14e8e6129c6eac1c37db63
Collin Miller <colljmiller@gmail.com>
2026-03-24 16:47:56 -0500

    chore: resolving project vunls and peerDeps issues (#40)

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,11 @@
 const js = require('@eslint/js');
-const { FlatCompat } = require('@eslint/eslintrc');
 const typescriptEslint = require('@typescript-eslint/eslint-plugin');
 const typescriptParser = require('@typescript-eslint/parser');
-const importPlugin = require('eslint-plugin-import');
 const prettierPlugin = require('eslint-plugin-prettier');
 const prettierConfig = require('eslint-config-prettier');
 
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-});
-
 module.exports = [
-    ...compat.extends('airbnb-base', 'eslint-config-prettier', 'plugin:prettier/recommended'),
+    js.configs.recommended,
     {
         files: ['**/*.ts'],
         languageOptions: {
@@ -21,34 +14,27 @@ module.exports = [
                 ecmaVersion: 12,
                 sourceType: 'module',
             },
+            globals: {
+                Buffer: 'readonly',
+                console: 'readonly',
+                process: 'readonly',
+                __dirname: 'readonly',
+                __filename: 'readonly',
+            },
         },
         plugins: {
             '@typescript-eslint': typescriptEslint,
         },
         rules: {
-            // note you must disable the base rule as it can report incorrect errors
+            // TypeScript ESLint recommended rules
+            ...typescriptEslint.configs.recommended.rules,
             'no-shadow': 'off',
             '@typescript-eslint/no-shadow': ['error'],
             // note you must disable the base rule as it can report incorrect errors
             'no-unused-vars': 'off',
             '@typescript-eslint/no-unused-vars': ['error'],
-            'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-            'import/extensions': [
-                'error',
-                'ignorePackages',
-                {
-                    js: 'never',
-                    jsx: 'never',
-                    ts: 'never',
-                    tsx: 'never',
-                },
-            ],
         },
-        settings: {
-            'import/resolver': {
-                typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
-            },
-        },
+        settings: {},
         ignores: ['.github/**', 'node_modules/**', 'dist/**', 'lib/**'],
     },
 ];
